86f00056c10daa4f4c1dd1911a13531746236d77
Jose Reimondez <josereimondez29@gmail.com>
2024-02-19 20:51:24 +0000

    finitto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const home = () => {
+const Home = () => {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState('');
 
+  useEffect(() => {
+    const urlTodos = "https://playground.4geeks.com/apis/fake/todos/user/josereimondez29";
+    
+    fetch(urlTodos)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setTodos(data);
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }, []);
+
   const addTodo = () => {
     if (inputValue) {
-      setTodos([...todos, inputValue]);
+      const newTodo = {
+        label: inputValue,
+        done: false
+      };
+      setTodos([...todos, newTodo]);
       setInputValue('');
     }
   };
@@ -31,17 +53,16 @@ const home = () => {
       <ul>
         {todos.map((todo, index) => (
           <li key={index}>
-            {todo}
-            <button class="delete-button" onClick={() => handleDelete(index)}><i class="fa-solid fa-trash"></i></button>
+            {todo.label}
+            <button className="delete-button" onClick={() => handleDelete(index)}><i className="fa-solid fa-trash"></i></button>
           </li>
         ))}
       </ul>
       <div className='Counter'>
-        {todos.length === 0 ? (<p>No Duty</p>) : (`${todos.length} duty to do!`)}
+        {todos.length === 0 ? (<p>No Duty</p>) : (`${todos.length} task`)}
       </div>
     </div>
   );
 };
 
-export default home;
-
+export default Home;
